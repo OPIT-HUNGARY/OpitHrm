6ccd563ad297bc6a4f36c553117a090d8beb8f98
Manish Chawla <chawla@opit.hu>
2013-11-25 17:10:49 +0100

    FIX: New Migration version generated(employeeName was missing)

---
+++ app/DoctrineMigrations/Version20131125170325.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20131125170325 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE notes_users ADD employeeName VARCHAR(25) NOT NULL");
+        $this->addSql("CREATE UNIQUE INDEX UNIQ_8E744D49BAFFBE7F ON notes_users (employeeName)");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("DROP INDEX UNIQ_8E744D49BAFFBE7F ON notes_users");
+        $this->addSql("ALTER TABLE notes_users DROP employeeName");
+    }
+}
