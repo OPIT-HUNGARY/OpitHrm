7aeb5848619dacbcf847c1f15db894fcd5b5d8b7
Marton Kaufmann <kaufmann@opit.hu>
2014-03-07 16:09:17 +0100

    FIX: Migration schema mismatch bugfix
    When generating the migration file for the first time
    the local schema had not been in sync.
    Only required schema modifications are kept in the file.

--- app/DoctrineMigrations/Version20140307151831.php
+++ app/DoctrineMigrations/Version20140307151831.php
@@ -15,7 +15,6 @@ class Version20140307151831 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("ALTER TABLE notes_travel_expense ADD CONSTRAINT FK_45CCB7FD4663DE14 FOREIGN KEY (travelRequest_id) REFERENCES notes_travel_request (id)");
         $this->addSql("ALTER TABLE notes_travel_request CHANGE opportunity_name customer_name VARCHAR(255) DEFAULT NULL");
     }
 
@@ -24,7 +23,6 @@ class Version20140307151831 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("ALTER TABLE notes_travel_expense DROP FOREIGN KEY FK_45CCB7FD4663DE14");
         $this->addSql("ALTER TABLE notes_travel_request CHANGE customer_name opportunity_name VARCHAR(255) DEFAULT NULL");
     }
 }
