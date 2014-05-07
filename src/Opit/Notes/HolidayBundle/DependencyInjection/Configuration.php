a819cca001f033d5cfec80c4c754f18ce1301745
Laszlo Bota <bota@opit.hu>
2014-05-07 16:59:21 +0200

    Config setting - E/D Leave Plan (#4917)
    The composer install command should be run to refresh the configuration.
    1) The configuration parameters were added to the config.yml of
       the Holiday bundle.
    2) The configuration file of the DependecnyInjection folder of the Holiday
       bundle was modified. The root node was added.
    3) config of App was modified. The imports field was extended by the
       Holiday bundle's config file.
    
    Signed-off-by: Laszlo Bota <bota@opit.hu>

--- src/Opit/Notes/HolidayBundle/DependencyInjection/Configuration.php
+++ src/Opit/Notes/HolidayBundle/DependencyInjection/Configuration.php
@@ -20,9 +20,18 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('opit_notes_holiday');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->arrayNode('leave_entitlement_plan')
+                    ->canBeEnabled()
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('default_days')
+                            ->defaultValue('%leave_default_days%')
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
