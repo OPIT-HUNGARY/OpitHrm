3b1293c9925599808246bbe8fda063b93f574f2c
Sven Henneboele <henneboele@opit.hu>
2014-05-27 14:53:11 +0200

    Composer ScriptHandler refacortization (#5158)
    ScriptHandler class is moved to the component and
    composer.json updated.

--- src/Opit/Component/Composer/ScriptHandler.php
+++ src/Opit/Component/Composer/ScriptHandler.php
@@ -2,14 +2,14 @@
 
 /*
  *  This file is part of the {Bundle}.
- * 
+ *
  *  (c) Opit Consulting Kft. <info@opit.hu>
- * 
+ *
  *  For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
  */
 
-namespace Opit\Notes\UserBundle\Composer;
+namespace Opit\Component\Composer;
 
 use Composer\Script\CommandEvent;
 use Symfony\Component\Yaml\Parser;
