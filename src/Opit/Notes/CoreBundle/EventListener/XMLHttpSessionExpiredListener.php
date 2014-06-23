d21f0d01212f488364076d6bea797025e3c6f2f0
Sven Henneboele <henneboele@opit.hu>
2014-06-23 15:04:34 +0200

    FIX: XML HTTP session listener affiliation update
    The xmlhttp listener is moved to the core bundle.

--- src/Opit/Notes/CoreBundle/EventListener/XMLHttpSessionExpiredListener.php
+++ src/Opit/Notes/CoreBundle/EventListener/XMLHttpSessionExpiredListener.php
@@ -2,25 +2,25 @@
 
 /*
  *  This file is part of the {Bundle}.
- * 
+ *
  *  (c) Opit Consulting Kft. <info@opit.hu>
- * 
+ *
  *  For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
  */
 
-namespace Opit\Notes\TravelBundle\EventListener;
+namespace Opit\Notes\CoreBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernel;
 
 /**
- * Description of NotificationExceptionListener
+ * Description of XMLHttpSessionExpiredListener
  *
  * @author OPIT Consulting Kft. - PHP Team - {@link http://www.opit.hu}
  * @version 1.0
- * @package Notes
- * @subpackage TravelBundle
+ * @package Opit
+ * @subpackage Notes
  */
 class XMLHttpSessionExpiredListener
 {
@@ -30,7 +30,7 @@ class XMLHttpSessionExpiredListener
             // don't do anything if it's not the master request
             return;
         }
-        
+
         if (false === strpos($event->getRequest()->getRequestUri(), 'login')) {
             if ($event->getRequest()->isXmlHttpRequest() && $event->getResponse()->getStatusCode() == "302") {
                 $event->getResponse()->setStatusCode(401);
@@ -39,6 +39,6 @@ class XMLHttpSessionExpiredListener
                 $event->setResponse($response);
             }
         }
-        
+
     }
 }
