3a79615448e009b2bac622e3084d7fdd8a649421
Sven Henneboele <henneboele@opit.hu>
2014-06-24 10:19:55 +0200

    Mass leave request range bugfixes (#5514)
    The mass leave request fixture was calculating the
    date range based on the current year. A more general
    approach, applying to any year is implemented.
    Leave request service used for day count calculation.
    Order of request creation change to insert mass request
    first.
    Added states reduced to insert approved only.
    Created user for individual requests changed to GM.

--- src/Opit/Notes/LeaveBundle/DataFixtures/ORM/MassLeaveRequestFixtures.php
+++ src/Opit/Notes/LeaveBundle/DataFixtures/ORM/MassLeaveRequestFixtures.php
@@ -38,21 +38,22 @@ class MassLeaveRequestFixtures extends AbstractDataFixture
             throw new \RuntimeException('Mass leave request fixtures require user fixtures.');
         }
 
+        $service = $this->container->get('opit.model.leave_request');
         $admin = $this->getReference('admin');
 
         // Create Datetimes.
-        $startDate = new \DateTime(date('Y-12-27'));
-        $startDate->modify('next monday');
-        $endDate = clone $startDate;
-        $endDate->add(new \DateInterval('P4D'));
-        $days = $endDate->diff($startDate);
+        $startDate = new \DateTime(date('Y-12-24'));
+        $endDate = new \DateTime(date('Y-01-01'));
+        $endDate->modify('+1 year');
+        // Notice: The service is based on LeaveDatesFixtures, ensure current year's public holidays are present.
+        $days = $service->countLeaveDays($startDate, $endDate);
 
         // Create leave
         $leave = new Leave();
         $leave->setDescription('Christmas vacation');
         $leave->setStartDate($startDate);
         $leave->setEndDate($endDate);
-        $leave->setNumberOfDays($days->format('%a'));
+        $leave->setNumberOfDays($days);
         $leave->setCategory($this->getReference('leave-category-full-day'));
 
         // Create leave request group.
@@ -62,60 +63,41 @@ class MassLeaveRequestFixtures extends AbstractDataFixture
         // Create First LeaveRequest.
         $leaveRequest1 = new LeaveRequest();
         $leaveRequest1->setEmployee($admin->getEmployee());
+        $leaveRequest1->setCreatedUser($this->getReference('generalManager'));
         $leaveRequest1->setGeneralManager($this->getReference('generalManager'));
-        $leaveRequest1->setCreatedUser($admin);
         $leaveRequest1->setIsMassLeaveRequest(false);
         $leaveRequest1->setLeaveRequestGroup($leaveRequestGroup);
         $leaveRequest1->addLeaf($leave);
 
-        // Fake the state times
-        $createdDateTime = new \DateTime();
-        $createdDateTime->modify('last year dec last weekday');
-        $createdDateTime->setTime('09', '45', '00');
-        $forApprovalDateTime = clone $createdDateTime;
-        $forApprovalDateTime->add(new \DateInterval('PT1H'));
-        $approvedDateTime = clone $forApprovalDateTime;
-        $approvedDateTime->modify('next weekday 16:13:00');
+        // Create mass leave request, persist first.
+        $massLeaveRequest = clone $leaveRequest1;
+        $massLeaveRequest->setEmployee($this->getReference('generalManager')->getEmployee());
+        $massLeaveRequest->setCreatedUser($this->getReference('generalManager'));
+        $massLeaveRequest->setIsMassLeaveRequest(true);
+        $massLeaveRequest->setLeaveRequestGroup($leaveRequestGroup);
+        $massLeaveRequest->addLeaf(clone $leave);
+
+        $manager->persist($massLeaveRequest);
 
         // Add leave request states
-        $leaveRequest1Status = new StatesLeaveRequests($this->getReference('created'));
-        $leaveRequest1Status->setCreated($createdDateTime);
-        $leaveRequest1Status->setCreatedUser($admin);
+        $leaveRequest1Status = new StatesLeaveRequests($this->getReference('approved'));
+        $leaveRequest1Status->setCreated(new \DateTime());
+        $leaveRequest1Status->setCreatedUser($this->getReference('generalManager'));
         $leaveRequest1->addState($leaveRequest1Status);
-
-        $leaveRequest2Status = new StatesLeaveRequests($this->getReference('forApproval'));
-        $leaveRequest2Status->setCreated($forApprovalDateTime);
-        $leaveRequest2Status->setCreatedUser($admin);
-        $leaveRequest1->addState($leaveRequest2Status);
-
-        $leaveRequest3Status = new StatesLeaveRequests($this->getReference('approved'));
-        $leaveRequest3Status->setCreated($approvedDateTime);
-        $leaveRequest3Status->setCreatedUser($this->getReference('generalManager'));
-        $leaveRequest1->addState($leaveRequest3Status);
 
         $manager->persist($leaveRequest1);
 
         // Create Second leave request.
         $leaveRequest2 = clone $leaveRequest1;
         $leaveRequest2->setEmployee($this->getReference('user')->getEmployee());
+        $leaveRequest2->setCreatedUser($this->getReference('generalManager'));
         $leaveRequest2->setIsMassLeaveRequest(false);
         $leaveRequest2->setLeaveRequestGroup($leaveRequestGroup);
         $leaveRequest2->addLeaf(clone $leave);
 
         $leaveRequest2->addState(clone $leaveRequest1Status);
-        $leaveRequest2->addState(clone $leaveRequest2Status);
-        $leaveRequest2->addState(clone $leaveRequest3Status);
 
         $manager->persist($leaveRequest2);
-
-        // Create Mass leave request.
-        $massLeaveRequest = clone $leaveRequest1;
-        $massLeaveRequest->setEmployee($this->getReference('generalManager')->getEmployee());
-        $massLeaveRequest->setIsMassLeaveRequest(true);
-        $massLeaveRequest->setLeaveRequestGroup($leaveRequestGroup);
-        $massLeaveRequest->addLeaf(clone $leave);
-
-        $manager->persist($massLeaveRequest);
 
         $manager->flush();
     }
