e9bf98e5dc96a0add1d0e2645570856df378517d
Sven Henneboele <henneboele@opit.hu>
2014-08-19 13:34:20 +0200

    First login listener bugfixes (#6195)
    Checking the currents request path changed to getPathInfo.
    getBasePath was not sufficient to check on the secured area path.
    New check on user token added, bypassing the listener if not a
    UsernamePasswordToken.
    Token condition fixed to support anonymous token.
    Listener extended to bypass if user ldap authentication is enabled.

--- src/Opit/OpitHrm/UserBundle/EventListener/FirstLoginListener.php
+++ src/Opit/OpitHrm/UserBundle/EventListener/FirstLoginListener.php
@@ -2,9 +2,9 @@
 
 /*
  *  This file is part of the OPIT-HRM project.
- * 
+ *
  *  (c) Opit Consulting Kft. <info@opit.hu>
- * 
+ *
  *  For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
  */
@@ -16,10 +16,11 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Routing\Router;
 use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 /**
  * Description of FirstLoginListener
- * 
+ *
  * @author OPIT Consulting Kft. - PHP Team - {@link http://www.opit.hu}
  * @version 1.0
  * @package OPIT-HRM
@@ -29,48 +30,58 @@ class FirstLoginListener
 {
     private $securityContext;
     private $router;
-    
+
     /**
-     * 
+     *
      * @param \Symfony\Component\Security\Core\SecurityContext $securityContext
      * @param \Symfony\Component\Routing\Router $router
      * @param \Symfony\Component\DependencyInjection\Container $container
      */
     public function __construct(SecurityContext $securityContext, Router $router)
     {
-	$this->securityContext = $securityContext;
+        $this->securityContext = $securityContext;
         $this->router = $router;
-	}
-    
+    }
+
     /**
      * If users flag isFirstLogin is true redirect to change password page.
-     * 
+     *
      * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
         // Don't do anything if it's not the master request or another firewall than "secured"
-        $isSecuredArea = (bool) preg_match('/^\/secured/', $event->getRequest()->getBasePath());
+        $isSecuredArea = (bool) preg_match('/^\/secured/', $event->getRequest()->getPathInfo());
         if (HttpKernel::MASTER_REQUEST != $event->getRequestType() || !$isSecuredArea) {
             return;
         }
-        
+
         $token = $this->securityContext->getToken();
+
+        // Bypass listener if token does not match UsernamePasswordToken
+        if (!$token instanceof UsernamePasswordToken) {
+            return;
+        }
+
         $changePasswordUrl = $this->router->generate('OpitOpitHrmUserBundle_user_change_password', array(), true);
         $excludedRoutes = array(
             $this->router->generate('OpitOpitHrmUserBundle_logout', array(), true),
             $changePasswordUrl
         );
-        
-        if (null !== $token) {
+
+        if (null !== $token && is_object($token->getUser())) {
+            // Bypass the change password page if user has ldap auth enabled
+            if ($token->getUser()->isLdapEnabled()) {
+                return;
+            }
+
             if (in_array($event->getRequest()->getUri(), $excludedRoutes)) {
                 return;
             }
-            
+
             if ($token->getUser()->getIsFirstLogin()) {
                 $event->setResponse(new RedirectResponse($changePasswordUrl));
             }
         }
-        
     }
 }
